69b2b57dd2560fc22fae4945c49858447a28a35e
Alexandre COLLIGNON <alexandre@collign.net>
2012-10-04 21:50:43 +0200

    Add Form and constraints.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,17 @@
 package controllers;
 
 import play.*;
+import play.data.*;
 import play.mvc.*;
 
 import views.html.*;
 
+import models.Task;
+
 public class Application extends Controller {
-  
+
+  static Form<Task> taskForm = form(Task.class);
+
   public static Result index() {
     return redirect(routes.Application.tasks());
   }
